45af2d4283bfbe7ffa523e0c6892149b60b67f75
Avery Myers <averyjmyers@hotmail.com>
2023-09-20 15:48:10 -0700

    updated reaction model

--- models/reaction.js
+++ models/reaction.js
@@ -1,7 +1,23 @@
 const { Schema, Types } = require('mongoose');
 
-const reactionSchema = new Schema({
-    user
-})
-
+const reactionSchema = newSchema({
+    reactionId: {
+        type: ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody:  {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type:String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+}
+)
 const userSchema = new Schema
