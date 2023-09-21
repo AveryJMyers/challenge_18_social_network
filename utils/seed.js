ed37fcaa4e75e11a3c117e9cec4b8d56b19f0304
Avery Myers <averyjmyers@hotmail.com>
2023-09-21 16:41:22 -0700

    updated index, and seeds

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,50 @@
-const connection = require('../config/connection')
+const connection = require('../config/connection');
 const { User, Reaction, Thought } = require('../models');
+const { userData, reactionData, thoughtData } = require('./data'); // Import your data
 
+connection.on('error', (err) => {
+  console.error('Database connection error:', err);
+});
 
-connection.on('error', (err) => err);
+connection.once('open', async () => {
+  try {
+    console.log('Connected to the database');
+
+    // Drop collections if they exist
+    await Promise.all([
+      User.deleteMany(),
+      Reaction.deleteMany(),
+      Thought.deleteMany(),
+    ]);
+
+    // Seed users
+    const users = await User.insertMany(userData);
+
+    // Map usernames to their respective IDs
+    const userMap = {};
+    users.forEach((user) => {
+      userMap[user.username] = user._id;
+    });
+
+    // Seed reactions
+    const reactions = reactionData.map((reaction) => ({
+      ...reaction,
+      user: userMap[reaction.username],
+    }));
+    await Reaction.insertMany(reactions);
+
+    // Seed thoughts
+    const thoughts = thoughtData.map((thought) => ({
+      ...thought,
+      user: userMap[thought.username],
+    }));
+    await Thought.insertMany(thoughts);
+
+    console.log('Database seeded successfully!');
+  } catch (error) {
+    console.error('Error seeding database:', error);
+  } finally {
+    // Close the database connection
+    connection.close();
+  }
+});
