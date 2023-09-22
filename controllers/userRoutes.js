07edb07bfc1d22dd5c0e04c8c01f0dc315983e43
Avery Myers <averyjmyers@hotmail.com>
2023-09-21 17:39:00 -0700

    updated some user routes

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,11 +1,27 @@
 const {User, Thought, Routes} = require('../models')
-
+module.exports = {
 //get all users
-
-
-// get user by id
-
-
+    async getAllUsers(req, res) {
+        try{
+            const allUsers = await User.find();
+            res.json(allUsers);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // get user by id
+    async getOneUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v');
+            if (!user) {
+              return res.status(404).json({ message: 'No user with that ID' });
+            }
+            res.json(user);
+          } catch (err) {
+            res.status(500).json(err);
+          }
+        },
 // create new user
 
 
@@ -13,3 +29,4 @@ const {User, Thought, Routes} = require('../models')
 
 
 // delete user by id
+}
